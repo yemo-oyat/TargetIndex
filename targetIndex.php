e2e280561219d19b569038e93ad8c496c79018d6
yemo-oyat <adebayoryemi@gmail.com>
2021-10-26 11:13:12 +0100

    Init commit

---
+++ targetIndex.php
@@ -0,0 +1,36 @@
+<?php
+
+function targetIndex($array, $target) {
+    $nearestValue = 0;
+    $left = 0;
+    $right = count($array) - 1;
+    $arrayLength = count($array);
+    for ($n = 0; $n < $arrayLength; $n++) {        
+        if (abs($target - $nearestValue) > abs( $target - $array[$n])) {
+            $nearestValue = $array[$n]; // Update the value next to the Target
+            $t = abs( $target - $array[$n]);
+            $s = abs($target - $nearestValue);
+        }
+        if ($target < $nearestValue) {
+            $nearestValue = $indexOfTarget +1;  // Update the value next to the Target
+        }
+        elseif ($target > $nearestValue) {
+            $indexOfTarget = array_search($nearestValue, $array);
+            $indexOfTarget = $indexOfTarget +1;
+        }else {
+            $indexOfTarget = array_search($nearestValue, $array);
+        }        
+    }
+    echo "Index of Target = ".$indexOfTarget." Nearest value".$nearestValue;
+}
+
+$array = array(1,2,4,6,10);
+// $target = 12;
+// $target = 3;
+// $target = 8;
+// $target = 9;
+// $target = 5;
+$target = 2;
+targetIndex($array, $target);
+
+?>
